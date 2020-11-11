f6ac40901cc9676e6d93ed56eaf77622da5c522f
Siddharth Natrajan <sidddharthvn2014@gmail.com>
2020-11-11 11:11:19 +0530

    Tailwind Setup

--- src/App.js
+++ src/App.js
@@ -1,19 +1,12 @@
-import "./App.css";
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import React from "react";
+class App extends React.Component {
+  render() {
+    return (
+      <div className="bg-black h-screen">
+        <div className="text-white font-bold text-6xl">Hello World</div>
+      </div>
+    );
+  }
 }
 
 export default App;
