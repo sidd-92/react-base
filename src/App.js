7060d7ddf0c81183460fc2a5f570eca2420c01f7
sidd-92 <sidddharthvn2014@gmail.com>
2021-01-05 12:50:15 +0530

    Play Sounds

--- src/App.js
+++ src/App.js
@@ -2,11 +2,33 @@ import React from "react";
 import AlarmCard from "./components/AlarmCard/AlarmCard";
 import { Button } from "primereact/button";
 import FormDialog from "./components/FormDialog/FormDialog";
+import Sound from "react-sound";
+import { Dropdown } from "primereact/dropdown";
+import alarm1 from "./assets/sound/alarm1.wav";
+import alarm2 from "./assets/sound/alarm2.mp3";
+import alarm3 from "./assets/sound/alarm3.wav";
 class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
       isFormDialogVisible: false,
+      playSound: "STOPPED",
+      soundIsPlaying: false,
+      selectedAlarm: "",
+      alarmOptions: [
+        {
+          label: "Alarm1",
+          value: alarm1,
+        },
+        {
+          label: "Alarm2",
+          value: alarm2,
+        },
+        {
+          label: "Alarm3",
+          value: alarm3,
+        },
+      ],
     };
   }
   render() {
@@ -33,7 +55,35 @@ class App extends React.Component {
             label="Add Alarm"
             className="p-button-raised p-button-rounded"
           />
+          <Button
+            disabled={
+              this.state.soundIsPlaying || this.state.selectedAlarm === ""
+            }
+            onClick={() => this.setState({ playSound: "PLAYING" })}
+            label="Play Alarm"
+            className="p-button-raised p-button-rounded"
+          />
         </div>
+        <Dropdown
+          value={this.state.selectedAlarm}
+          options={this.state.alarmOptions}
+          onChange={(e) => {
+            this.setState({ selectedAlarm: e.value });
+          }}
+          placeholder="Select a Alarm"
+        />
+        <Sound
+          url={this.state.selectedAlarm}
+          playStatus={this.state.playSound}
+          onPlaying={() => this.setState({ soundIsPlaying: true })}
+          onFinishedPlaying={() =>
+            this.setState({ soundIsPlaying: false, playSound: "STOPPED" })
+          }
+          onError={() => {
+            console.log("Error");
+            alert("Error");
+          }}
+        />
         <div style={{ height: "600px" }} className="overflow-scroll mx-1">
           <AlarmCard title="Alarm 1">
             <div>Hello</div>
