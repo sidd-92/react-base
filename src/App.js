9e9d642ece7b916d9954e37f52e23c2d3ae52526
Siddharth Natrajan <sidddharthvn2014@gmail.com>
2021-01-13 12:28:41 +0530

    Changed Manifest

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import { Dialog } from "primereact/dialog";
 import { InputText } from "primereact/inputtext";
 import { InputTextarea } from "primereact/inputtextarea";
 import { Button } from "primereact/button";
+import { Card } from "primereact/card";
 class App extends React.Component {
   constructor(props) {
     super(props);
@@ -156,7 +157,17 @@ class App extends React.Component {
   }
 
   render() {
-    return <div className="font-bold text-5xl">Expense App</div>;
+    return (
+      <div className="m-1">
+        <div className="font-bold text-3xl">Expense App</div>
+        <div className="w-full">
+          <Card>Content</Card>
+        </div>
+        <div className="fixed bottom-0 right-0 m-4">
+          <Button icon="pi pi-plus" label="Add" />
+        </div>
+      </div>
+    );
   }
 }
 
